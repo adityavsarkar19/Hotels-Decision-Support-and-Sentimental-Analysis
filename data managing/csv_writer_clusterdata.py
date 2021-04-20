859308ab51f8f9b6b1710fc15e69bf180b30492b
Pritish Dugar <pritishdugar509@gmail.com>
2021-04-20 16:25:30 +0700

    csv writer

--- data managing/csv_writer_clusterdata.py
+++ data managing/csv_writer_clusterdata.py
@@ -2,10 +2,9 @@ import csv
 import json
 
 def numbers_to_csv(data):
-    row_list = [["avg_rating", "comment_avg_rating",]]
+    row_list = [["avg_rating", "comment_avg_rating"]]
     for i in data.keys():
         row = []
-        row.append(data[i]['avg_rating'])
         row.append(data[i]['comment_rating'])
         row_list.append(row)
     return row_list
@@ -16,6 +15,6 @@ with open(f"{filename}.json") as f:
 
 row_list = numbers_to_csv(data)
 
-with open(f'{filename}_cluster.csv', 'w', newline='') as file:
+with open(f'clustering/{filename}_cluster.csv', 'w', newline='') as file:
     writer = csv.writer(file)
     writer.writerows(row_list)

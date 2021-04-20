0874056779451ca54da4e6a3940ac6057390ee67
Pritish Dugar <pritishdugar509@gmail.com>
2021-04-20 12:49:23 +0700

    files to manage data

---
+++ data managing/csv_writer_clusterdata.py
@@ -0,0 +1,21 @@
+import csv
+import json
+
+def numbers_to_csv(data):
+    row_list = [["avg_rating", "comment_avg_rating",]]
+    for i in data.keys():
+        row = []
+        row.append(data[i]['avg_rating'])
+        row.append(data[i]['comment_rating'])
+        row_list.append(row)
+    return row_list
+
+filename = "Kuala_Lumpur"
+with open(f"{filename}.json") as f:
+        data = json.load(f)
+
+row_list = numbers_to_csv(data)
+
+with open(f'{filename}_cluster.csv', 'w', newline='') as file:
+    writer = csv.writer(file)
+    writer.writerows(row_list)

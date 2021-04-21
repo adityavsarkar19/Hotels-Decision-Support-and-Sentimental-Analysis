182e3e14b36707e3be3e9c83360610a517858b44
Aditya Sarkar <adityavsarkar@gmail.com>
2021-04-22 04:37:35 +0530

    map plot code added but wrong coords

---
+++ Map Plots/top10_polaritymaps/map_plotting1.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Apr 22 04:17:44 2021
+
+@author: Aditya Sarkar
+"""
+
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+
+
+df = pd.read_csv('Bangkok_Polarity_Coords.csv')
+
+#Calculating the Bounding Box Values
+BBox = ((df.Longitudes.min(),   df.Longitudes.max(),      
+         df.Latitudes.min(), df.Latitudes.max()))

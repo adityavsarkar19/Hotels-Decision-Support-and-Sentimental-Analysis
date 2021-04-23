d98d4a00bb8347bdee770eada8916848a974d9ad
Aditya Sarkar <adityavsarkar@gmail.com>
2021-04-23 08:45:40 +0530

    all sentiment plots added

--- Sentimental Analysis/vis.py
+++ Sentimental Analysis/vis.py
@@ -9,7 +9,7 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import numpy as np
 
-df = pd.read_csv("Bangkok_cluster.csv")
+df = pd.read_csv("Kuala_Lumpur_cluster.csv")
 names = df['Hotels'].values
 x = np.arange(len(names))
 w = 0.3
@@ -18,7 +18,7 @@ plt.bar(x, df['polarity_score'].values, width=w, label='Polarity Scores')
 plt.xticks(x, names)
 plt.ylim([0,10])
 plt.tight_layout()
-plt.xlabel('Bangkok Hotels Analysis')
+plt.xlabel('Kuala Lumpur Hotels Analysis')
 plt.legend(loc='upper center', bbox_to_anchor=(0.5, -0.1), fancybox=True, ncol=5)
-plt.savefig("CSVBarplots.png", bbox_inches="tight")
+plt.savefig("Kuala_Lumpur_plot.png", bbox_inches="tight")
 plt.show()
